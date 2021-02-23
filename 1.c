06fd49d6d2784326c89cff02e0f91dc4903cf2b3
CyTon Program Languages <cytoncode@gmail.com>
2021-02-23 16:33:47 +0200

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,2 +1,40 @@
-#include <cmath>
-y = pow(sin(a*x), 3) + b  / pow(cos(x), 2)
+#include <stdio.h>
+#include <math.h>
+
+/*  test:
+-3.8
+0.5
+0.08
+*/
+
+
+double input(char* value)
+{
+double n;
+printf("%s", value);
+scanf("%lf\n", &n);
+return n;
+}
+
+
+int main()
+{
+double x, y, a, b, c;
+
+x = input("x = "),
+a = input("a = "),
+c = input("c = ");
+
+c = log(c),
+b = c + pow(x, 2),
+a = a*b;
+
+a = pow(sin(a*x), 3) + b;
+b = pow(cos(x), 2);
+
+y = a/b;
+
+printf("%lf\n", y);
+
+return 0;
+} 
