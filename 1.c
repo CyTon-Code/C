091b97994d11a8a73c7a59307383aacd81d914c2
CyTon Program Languages <cytoncode@gmail.com>
2021-02-23 17:37:56 +0200

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,40 +1,28 @@
 #include <stdio.h>
 #include <math.h>
 
-/*  test:
--3.8
-0.5
-0.08
-*/
+double input(char* value){
+    double n;
 
+    printf("%s", value);
+    scanf("%lf", &n);
+    printf("\n");
 
-double input(char* value)
-{
-double n;
-printf("%s", value);
-scanf("%lf\n", &n);
-return n;
+    return n;
 }
 
+int main(void){
+    double x, y, a, b, c;
 
-int main()
-{
-double x, y, a, b, c;
+    x = input("x = ");
+    a = input("a = ");
+    c = input("c = ");
 
-x = input("x = "),
-a = input("a = "),
-c = input("c = ");
-
-c = log(c),
-b = c + pow(x, 2),
-a = a*b;
-
-a = pow(sin(a*x), 3) + b;
-b = pow(cos(x), 2);
-
-y = a/b;
-
-printf("%lf\n", y);
-
-return 0;
-} 
+    c = log(c);
+    b = c + pow(x, 2);
+    a = a*b;
+    y = pow(sin(a*x), 3) + b /  pow(cos(x), 2);
+    
+    printf("%lf\n", y);
+    return 0;
+}
