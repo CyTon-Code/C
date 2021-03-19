1f44641244f26f611431c12d66c4011261a92f15
CyTon Code <cytoncode@gmail.com>
2021-03-19 11:11:37 +0200

    upd all

--- c_math.h
+++ c_math.h
@@ -1,22 +1,23 @@
 #include <stdio.h>
 #include <math.h>
 
-void input(const char* value, double n){
+void input(const char *value, double n) {
     printf("%s", value);  // вывод
     scanf("%lf", &n);  // ввод
     printf("\n");  // вывод
 }
 
+void swap(int a, int b);
+
 long long E = 2.718281828459045;
 //double El = 2.7182818284590452353602874713526624977572;
 
-double POW(long long n, double m)
-{
+double POW(long long n, double m) {
     double j = 1;  // изначательное число.
-    for (int i = 0; i <= m; i++, j*=n);  // разгон к нужному числу.
+    for (int i = 0; i <= m; i++, j *= n);  // разгон к нужному числу.
     return j;  // конечное число.
 }
-double EXP(double m)
-{
+
+double EXP(double m) {
     return POW(E, m);  // конечное число.
 }
