17d6f4f42d7a8f3360b7d0607af5f27d7faf63ac
CyTon Code <cytoncode@gmail.com>
2021-03-19 12:58:42 +0200

    upd all

---
+++ bool/cmake-build-debug/CMakeFiles/bool.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bool.dir/library.c.o"
+  "libbool.a"
+  "libbool.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/bool.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ bool/library.h
@@ -0,0 +1,71 @@
+#ifndef BOOL_LIBRARY_H
+#define BOOL_LIBRARY_H
+
+//false =  (char) 0
+//true =   (char) 1
+typedef struct {
+    char flag;
+//    int id;
+} boolean_type;
+
+int str_len(const char *text);
+
+/*
+ * функция преобразовывает число в bool
+ * */
+boolean_type convert_int_to_boolean_type(int number) {
+    boolean_type var;  // tmp boolean_type
+
+    // convert:
+    if (number)
+        var.flag = 1;
+    else
+        var.flag = 0;
+
+    return var;
+}
+
+boolean_type convert_char_to_boolean_type(char number) {
+    boolean_type var;
+    if (number)
+        var.flag = 1;
+    else
+        var.flag = 0;
+    return var;
+}
+
+boolean_type convert_string_to_boolean_type(const char *_text) {
+    char *key = "";
+    boolean_type var;
+    int length = str_len(key);
+
+    for (int i = 0; i < length; i++) {
+        if (key[i] != _text[i]) {
+            var.flag = 1;
+            return var;
+        }
+    }
+    var.flag = 0;
+    return var;
+}
+boolean_type convert_str_to_boolean_type(const char *_text) {
+    char *key = "";
+    boolean_type var;
+    int length = str_len(key);
+
+    for (int i = 0; i < length; i++) {
+        if (key[i] != _text[i]) {
+            var.flag = 1;
+            return var;
+        }
+    }
+    var.flag = 0;
+    return var;
+}
+int str_len(const char *text) {
+    int length = 0;
+    while (text[length++] != '\0');
+    return length;
+}
+
+#endif //BOOL_LIBRARY_H
