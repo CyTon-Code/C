2174f51355beb2c38e9171579bf3bfec963776ba
CyTon Code <cytoncode@gmail.com>
2021-03-19 11:23:32 +0200

    upd all

--- line_search/library.c
+++ line_search/library.c
@@ -1,6 +1,11 @@
 #include "library.h"
-
 #include <stdio.h>
+
+unsigned int LinSearch(int n, const int a[], int key) {
+    int i = 0;
+    while ((i < n) && (a[i] != key))i++;
+    return i;
+}
 
 void hello(void) {
     printf("Hello, World!\n");
--- line_search/library.h
+++ line_search/library.h
@@ -3,4 +3,6 @@
 
 void hello(void);
 
+unsigned int LinSearch(int n, const int a[], int key);
+
 #endif //LINE_SEARCH_LIBRARY_H
