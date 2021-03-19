964def80ce7cb7e41558112487ea2a481c6a7963
CyTon Code <cytoncode@gmail.com>
2021-03-19 11:11:51 +0200

    add all

---
+++ line_search/cmake-build-debug/CMakeFiles/line_search.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/line_search.dir/library.c.o"
+  "libline_search.a"
+  "libline_search.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/line_search.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ line_search/library.h
@@ -0,0 +1,6 @@
+#ifndef LINE_SEARCH_LIBRARY_H
+#define LINE_SEARCH_LIBRARY_H
+
+void hello(void);
+
+#endif //LINE_SEARCH_LIBRARY_H
